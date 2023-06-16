c21e3794b0be4a6db7025909f0220663daae3691
Patrik Bergman <38034479+ptrkbrgmn@users.noreply.github.com>
2023-06-16 12:07:01 +0200

    Add eslint

--- packages/academy/src/app.ts
+++ packages/academy/src/app.ts
@@ -1,2 +1,4 @@
 import "@fantastic/core";
+
+// eslint-disable-next-line no-console
 console.log("Hello from Academy!");
--- packages/event/src/app.ts
+++ packages/event/src/app.ts
@@ -1,2 +1,3 @@
 import "@nodet2/core";
+// eslint-disable-next-line no-console
 console.log("Hello from event!");
